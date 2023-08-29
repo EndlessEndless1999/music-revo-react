508aa52683aafca25d1ae0a0db83f52a8d04a197
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-08-29 12:19:11 +0100

    add card

---
+++ music_app/src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+export const Card = () => {
+
+
+    const openCard = () => {
+        return
+    }
+
+    const closeCard = () => {
+        return
+    }
+
+    return (
+        <>
+            <div className="card-container">
+                <div className='card-img'>
+                    <h3>Isnt Anything</h3>
+                    <button onClick={openCard}></button>
+                </div>
+                <div className='toggle-panel hidden'>
+                    <p className='card-description'></p>
+                    <ul className='track-list'></ul>
+                    <button type='submit' onClick={closeCard}>Close</button>
+                </div>
+            </div>
+        </>
+    )
+}
