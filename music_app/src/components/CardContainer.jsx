be63df5cc9d101c85ebcb8aa782b20814806c683
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-08-29 16:28:29 +0100

    add tracklist prop

--- music_app/src/components/CardContainer.jsx
+++ music_app/src/components/CardContainer.jsx
@@ -9,16 +9,19 @@ export default function CardContainer() {
             <Card title='Isnt Anything' image='card-img'>
                 <DropCard 
                 description="MBVs first album. Released in 1987. Would lay the foundations for their sound and shoegazing music as a whole."
+                tracklist={['Soft as Snow', 'Lose my breath', 'Cupid Come', 'Still in a Dream', 'No more sorry', 'All I need', 'Feed me with your kiss', 'SueisFine', 'Several Girls Galore', 'You never should', 'Nothing much to lose', 'I can see it' ]}
                 />
             </Card>
             <Card title='Loveless' image='card-img-loveless'>
                 <DropCard
                 description="MBVs second album, and most important. Would shape shoegazing and dream pop music for the 1990s and beyond."
+                tracklist={['Only Shallow', 'Loomer', 'Touched', 'To Here Knows When', 'When You Sleep', 'I Only Said', 'Come In Alone', 'Sometimes', 'Blown A Wish', 'What You Want', 'Soon']}
                 />
             </Card>
             <Card title='Eps' image='card-img-eps'>
                 <DropCard 
                 description="A collection of early EPs and single releases. Contains many essential tracks that are ommitted from the albums."
+                tracklist={['You made me realise', 'Slow', 'Thorn', 'Cigarette in your bed', 'Drive It All Over Me', 'I Believe', 'Emptiness Inside', 'I Need No Trust', 'Soon', 'Glider', 'Dont Ask Why', 'Off Your Face']}
                 />
             </Card>
         </div>
--- music_app/src/components/DropCard.jsx
+++ music_app/src/components/DropCard.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function DropCard(props) {
     const closeCard = () => {
         return
     }
 
+
+
     return (
     <div className='toggle-panel' id="toggle">
         <p className='card-description'>{props.description}</p>
-        <ListGroup>
-
-        </ListGroup>
+        <ul>
+            {props.tracklist.map((track, index) => (
+                <li key={props.tracklist[index]}>{props.tracklist[index]}</li>
+            ))}
+        </ul>
         <button type='submit' onClick={closeCard}>Close</button>
     </div>
     )
