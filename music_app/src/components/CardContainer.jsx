f728c3a0fe23260d74543c40ec42bd5fb6954f98
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-08-29 12:20:36 +0100

    add card container

---
+++ music_app/src/components/CardContainer.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Card from './Card';
+
+
+const CardContainer = () => {
+    return(
+        <div className="card-container">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
